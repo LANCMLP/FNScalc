b7e9791b0159776ecb52a6a30c06ecbc8d460776
Anthony Albanese <anthonya@legalaidnc.org>
2022-03-09 14:43:39 -0500

    a

--- shared/js/publish-results.js
+++ shared/js/publish-results.js
@@ -1,4 +1,2 @@
-import { results_tabs_text } from "./results-tabs";
-
-const WIC_results_text = results_tabs_text(WIC_results_text);
+export const WIC_results_text = results_tabs_text(WIC_results_text);
 document.getElementById("WIC-results-text").innerHTML = WIC_results_text;
